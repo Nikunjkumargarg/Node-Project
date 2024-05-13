f98d9ca42b4e68a0c8a66ef68a64d6e65063e6a2
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-13 22:01:41 +0530

    routing

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Hello javascript</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("<html>");
+    return res.end();
+  }
   res.write("<html>");
   res.write("<head><title>Hello javascript</title></head>");
   res.write("<body>Nikunj this side learning nodejs</body>");
