34c55d2e0a622ede0366e5c7d7aac3b25d92536e
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-13 21:44:55 +0530

    handson request and response object

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  res.write("<html>");
+  res.write("<head><title>Hello javascript</title></head>");
+  res.write("<body>Nikunj this side learning nodejs</body>");
+  res.write("<html>");
+  res.end();
 });
 
 server.listen(3000);
