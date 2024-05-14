a3c11b4ecd470e458be822e5a2eb895517aa8433
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-14 14:56:23 +0530

    body parser tested

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const http = require("http");
 const fs = require("fs");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("hello express");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/add-product", (req, res, next) => {
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="message">
+      <button type="submit">click</button>
+    </form>
+  `);
 });
-app.use((req, res, next) => {
-  console.log("middleware testing");
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
-app.listen(3000);
+
+app.use("/", (req, res, next) => {
+  console.log("hello from express");
+  res.send("Hello from Express!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
