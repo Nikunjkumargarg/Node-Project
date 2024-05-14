001c612cde4dbd39ec7714acf475b5dee39425c1
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-14 17:08:40 +0530

    404 html page added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const adminRouter = require("./router/adminRoutes");
 const shopRouter = require("./router/shopRoutes");
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRouter);
 app.use("/shop", shopRouter);
 app.use((req, res, next) => {
-  res.status(404).send(`<h1>Page not found</h1>`);
+  res.status(404).sendFile(path.join(__dirname, "views", "404page.html"));
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
