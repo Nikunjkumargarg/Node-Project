15314d8bd2c1806a6c28c52cd3b5290ed9f48204
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-21 01:46:49 +0530

    console message added

--- router/shopRoutes.js
+++ router/shopRoutes.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/products");
 router.get("/", productsController.getProducts);
-
+console.log("hello");
 module.exports = router;
+
