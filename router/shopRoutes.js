a6fef30fe7cae9697acdf5ab0feca62ae8f08330
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-22 19:20:40 +0530

    shopRoute file added one comment

--- router/shopRoutes.js
+++ router/shopRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/products");
+//get route
 router.get("/", productsController.getProducts);
 module.exports = router;
 
