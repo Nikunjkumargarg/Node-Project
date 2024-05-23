7d60e533671b90b2861a9d88a8af41f9eeae162a
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-23 17:29:43 +0530

    formatted app.js file

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const http = require("http");
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+const rootDir = require("./util/path");
+
 const adminRouter = require("./router/adminRoutes");
 const shopRouter = require("./router/shopRoutes");
 const errorController = require("./controllers/error");
-const path = require("path");
-const rootDir = require("./util/path");
+
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRouter);
 app.use("/shop", shopRouter);
+
 app.use(errorController.get404);
 //listening on port no. 3000
 app.listen(3000, () => {
