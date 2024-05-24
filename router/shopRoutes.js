3f19eecb9bbb834ef172bf3757c47a14ab350a03
nikunj kumar garg <nikunjkumargarg98@gmail.com>
2024-05-25 00:27:09 +0530

    shopRouter file modified

--- router/shopRoutes.js
+++ router/shopRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/products");
-//get route
 router.get("/", productsController.getProducts);
 module.exports = router;
 
